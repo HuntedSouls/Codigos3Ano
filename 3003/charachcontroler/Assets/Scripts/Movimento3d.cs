8b3c7d4f4262990ed37fb8d14e121db17c5a716c
HuntedSoulss <joshuakritz2@gmail.com>
2019-05-10 10:05:21 -0300

    Projetos de charachter controler

---
+++ 3003/charachcontroler/Assets/Scripts/Movimento3d.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento3d : MonoBehaviour {
+
+    CharacterController controle;
+    public float velocidade = 3.0f;
+
+    IEnumerator Pular()
+    {
+        Vector3 pulo = new Vector3(0, 5, 0);
+        controle.Move(pulo);
+        yield return null;
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        controle = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float direcaoX = Input.GetAxis("Horizontal");
+        float direcaoZ = Input.GetAxis("Vertical");
+        Vector3 movimento = new Vector3(direcaoX,0,direcaoZ);
+        controle.SimpleMove(movimento*velocidade);
+
+        if (Input.GetKeyDown(KeyCode.Space) && controle.isGrounded)
+        {
+            StartCoroutine(Pular());
+        }
+	}
+}
---
+++ 3004/chrachcontrol3004/Assets/Scripts/Movimento3d.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento3d : MonoBehaviour {
+
+    CharacterController controle;
+    float altura = 20.0f;
+    float gravidade = -30.0f;
+
+
+    // Use this for initialization
+    void Start () {
+        controle = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+        Vector3 movimento = new Vector3(movX,0,movZ);
+
+        if (Input.GetKeyDown(KeyCode.Space) && controle.isGrounded)
+        {
+            movimento.y += altura; 
+        }
+
+        movimento.y += gravidade*Time.deltaTime;
+
+        controle.Move(movimento*Time.deltaTime);
+	}
+}
